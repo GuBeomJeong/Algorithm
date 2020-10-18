83a1193f41ab5bbee0002e741291efd64787942d
GuBeomJeong <jgb625@naver.com>
2020-10-18 21:20:16 +0900

    [LEET-148] Sort List

---
+++ LeetCode/Medium/148. Sort List/148. Sort List.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    
+    ListNode* merge(ListNode* left, ListNode* right){
+        ListNode* start = new ListNode(0);
+        ListNode* pos = start;
+        
+        while(left && right){
+            if(left->val < right->val){
+                pos->next = left;
+                left = left->next;
+            }else{
+                pos->next = right;
+                right = right->next;
+            }
+            pos = pos->next;
+        }
+        
+        while(left){
+            pos->next = left;
+            left = left->next;
+            pos = pos->next;
+        }
+        
+        while(right){
+            pos->next = right;
+            right = right->next;
+            pos = pos->next;
+        }
+        
+        return start->next;
+    }
+    
+    ListNode* sortList(ListNode* head) {
+        
+        if(head == 0 || head->next == 0){
+            return head;
+        }
+        
+        ListNode* mid = head;
+        ListNode* end = head;
+        ListNode* pre = head;
+        
+        while(end && end->next){
+            pre = mid;
+            mid = mid->next;
+            end = end->next->next;
+        }
+        
+        pre->next = 0;
+        
+        ListNode* l1 = sortList(head);
+        ListNode* l2 = sortList(mid);
+        
+        return merge(l1,l2);
+    }
+    
+    
+};
